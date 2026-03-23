32f3ea95ece863f0bc70273fea66b4fed37709f7
shubham0419 <shubham.e21@nsut.ac.in>
2026-03-23 12:24:04 +0530

    fs module

---
+++ alpha/lect7/nodejsbasic/index.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+fs.writeFile("./first.txt","my second write operation","utf-8",(err)=>{
+  console.log(err);
+})
+
+fs.readFile("./first.txt","utf-8",(err,data)=>{
+  console.log(data);
+})
+
+// synchronous write
+console.log("sync operation");
+fs.writeFileSync("./first.txt","my third write operation","utf-8");
+
+// synchronous read
+const data = fs.readFileSync("./first.txt","utf-8");
+console.log(data);
+
+// to add data in file
+fs.appendFile("./first.txt","\nthis is appended text","utf-8",(err)=>{
+  console.log(err);
+})
+// to delete file after use
+fs.unlink("./first.txt",(err)=>{
+  console.log(err);
+})
