7e399f3612dcf5a5c5cd49d62156adcc54f8c7b3
shubham0419 <shubham.e21@nsut.ac.in>
2026-03-24 12:13:32 +0530

    fs module

---
+++ beta/lect7/nodejsbasics/index.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+// fs.writeFile("beta/lect7/nodejsbasics/index.js")      //relative path -> current directory path
+// fs.writeFile("/Users/shbha/Desktop/cb/mirai/beta/lect7/nodejsbasics/index.js")
+// path(absolute path)
+console.log("start");
+// to write in file -> this overwites the data
+fs.writeFile("beta/lect7/nodejsbasics/first.txt","hello this is line 1","utf-8",(err)=>{
+  console.log(err);
+})
+
+fs.readFile("beta/lect7/nodejsbasics/first.txt","utf-8",(err,data)=>{
+  console.log(data);
+})
+// to write in file -> this adds the data at last position
+fs.appendFile("beta/lect7/nodejsbasics/first.txt","\nhello this is line 2","utf-8",(err)=>{
+  console.log(err);
+})
+
+console.log("end");
