df6d3f8a364a32384848c9f136d1194f8e528ff3
shubham0419 <shubham.e21@nsut.ac.in>
2026-04-08 12:20:43 +0530

    server

--- beta/lect9/express/index.js
+++ beta/lect9/express/index.js
@@ -10,17 +10,7 @@ app.post("/",(req,res)=>{
   res.send("hello my server is live, method = post")
 })
 
-app.put("/",(req,res)=>{
-  res.send("hello my server is live, method = put")
-})
-
-app.patch("/",(req,res)=>{
-  res.send("hello my server is live, method = patch")
-})
-app.delete("/",(req,res)=>{
-  res.send("hello my server is live, method = delete")
-})
-
+// start server
 app.listen(PORT,()=>{
   console.log(`server is live at http://localhost:${PORT}`);
 })
