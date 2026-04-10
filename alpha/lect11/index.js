88e47b1296d2ccc686d8ad609eec48a449098f69
shubham0419 <shubham.e21@nsut.ac.in>
2026-04-10 10:26:06 +0530

    query,params,body

--- alpha/lect11/index.js
+++ alpha/lect11/index.js
@@ -2,11 +2,26 @@ const express = require("express")    //import express from node_modules
 const app = express()
 const PORT=4000;
 
+
+
 // query params
 app.get("/search",(req,res)=>{
   const query = req.query;
   console.log(query);
   res.send("query received",query)
+})
+
+app.get("/user/:id/data",(req,res)=>{
+  const params = req.params;
+  console.log(params);
+  res.send("params received",params);
+})
+// its a middleware to convert the encripted data in body back to json
+app.use(express.json())
+app.post("/user/create",(req,res)=>{
+  const body = req.body;
+  console.log(body);
+  res.send("body received",body);
 })
 
 // start server , port -> mapping of process in system
