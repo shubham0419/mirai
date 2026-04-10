bdc06df0a5603efbdbd79fc34d0fe6f4ed0a4383
shubham0419 <shubham.e21@nsut.ac.in>
2026-04-10 09:43:27 +0530

    numpy ,pandas

--- alpha/lect11/index.js
+++ alpha/lect11/index.js
@@ -2,10 +2,11 @@ const express = require("express")    //import express from node_modules
 const app = express()
 const PORT=4000;
 
+// query params
 app.get("/search",(req,res)=>{
   const query = req.query;
   console.log(query);
-  res.send("working on query")
+  res.send("query received",query)
 })
 
 // start server , port -> mapping of process in system
