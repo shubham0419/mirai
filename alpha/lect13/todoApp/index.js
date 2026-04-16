1c794d510873f5406a5b4fb9bcbc24a69a5d74fa
shubham0419 <shubham.e21@nsut.ac.in>
2026-04-16 10:45:57 +0530

    todo complete

--- alpha/lect13/todoApp/index.js
+++ alpha/lect13/todoApp/index.js
@@ -56,6 +56,38 @@ app.delete("/todo/:id/delete",(req,res)=>{
   }
 })
 
+app.put("/todo/:id/update",(req,res)=>{
+  try {
+    const id = req.params.id;
+    TODOS = TODOS.map((todo)=>{
+      if(todo.id === id){
+        todo.status = !todo.status;
+      }
+      return todo;
+    })
+  res.status(202).json({message:"Todo Updated",todos:TODOS})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message:error.message})
+  }
+})
+
+app.get("/todo/filter",(req,res)=>{
+  try {
+    const filter = req.query.filter;
+    const todos = TODOS.filter((todo)=>{
+      if(filter=="completed"){
+        return todo.status==false;
+      }
+      return todo.status !==false;
+    })
+  res.status(200).json({message:"Todo filtered",todos:todos})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message:error.message})
+  }
+})
+
 app.listen(PORT,()=>{
   console.log(`server is live at http://localhost:${PORT}`);
 })
--- alpha/lect13/todoApp/public/script.js
+++ alpha/lect13/todoApp/public/script.js
@@ -16,16 +16,55 @@ const createTodo = async (task)=>{
 }
 const container = document.getElementById("todo-container")
 const renderTodos = (todos)=>{
+  container.innerHTML = ""
   todos.forEach(todo => {
     const div = document.createElement("div");
     div.className = "todo";
     div.innerHTML = `<h3>${todo.task}</h3>
     <div id=${todo.id}>
-      <button>Complete</button>
-      <button>Delete</button>
+      <button class="complete">${todo.status?"Complete":"Undo"}</button>
+      <button class="delete">Delete</button>
     </div>`
     console.log(div);
     container.append(div);
   });
-  
+}
+
+container.addEventListener("click",(e)=>{
+  const id = e.target.parentElement.id;
+  if(e.target.className == "delete"){
+    deleteTodo(id)
+  }else if(e.target.className == "complete"){
+    updateTodo(id)
+  }
+})
+
+const updateTodo = async(id)=>{
+  let res = await axios.put(`http://localhost:4000/todo/${id}/update`);
+  const todos = res.data.todos;
+  renderTodos(todos);
+}
+
+const deleteTodo = async (id)=>{
+  let res = await axios.delete(`http://localhost:4000/todo/${id}/delete`);
+  const todos = res.data.todos;
+  renderTodos(todos);
+}
+
+const btns = document.getElementById("btns");
+btns.addEventListener("click",(e)=>{
+  const id = e.target.id;
+  if(id=="completed"){
+    filterTodo("completed")
+  }else if(id==="pending"){
+    filterTodo("pending")
+  }else if(id==="delte-completed"){
+    // impliment api and create function for api call
+  }
+})
+
+const filterTodo = async (filter) =>{
+  const res = await axios.get("http://localhost:4000/todo/filter",{params:{filter:filter}})
+  const todos = res.data.todos;
+  renderTodos(todos)
 }
